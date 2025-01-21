aaa5e71b06bb98cdb7381e1141ae1f8ea291ce6c
sebnis <sebincheriyan3@gmail.com>
2025-01-21 17:58:43 +0000

    latest update

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -2,11 +2,22 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.node, // Include Node.js globals
+        ...globals.mocha, // Include Mocha globals for testing (or jest if you are using Jest)
+      },
+    },
+    settings: {
+      react: {
+        version: "detect", // Automatically detect the React version
+      },
+    },
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
